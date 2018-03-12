787c3c3947710163747cae65c37c2fbd43b05f85
Yoimer <ydrf19@gmail.com>
2018-03-12 13:28:24 -0400

    008 add page content

--- public/index.php
+++ public/index.php
@@ -7,7 +7,14 @@
   <?php include(SHARED_PATH . '/public_navigation.php'); ?>
 
   <div id="page">
-
+    <?php
+      // Show the homepage
+      // The homepage content could:
+      //* be static content (here or in a shard file)
+      //* show the first page from the nav
+      //* be in the database but add code to hide in the nav
+      include(SHARED_PATH . '/static_homepage.php');
+    ?>
   </div>
 
 </div>
