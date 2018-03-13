a3c99119d4b522e3cfddacf285b8b07e10ca419f
Yoimer <ydrf19@gmail.com>
2018-03-13 15:15:17 -0400

    009 use navigation to select pages

--- public/index.php
+++ public/index.php
@@ -1,5 +1,17 @@
 <?php require_once('../private/initialize.php'); ?>
 
+<?php
+  if(isset($_GET['id'])) {
+    $page_id = $_GET['id'];
+    $page = find_page_by_id($page_id);
+    if(!$page) {
+      redirect_to(url_for('/index.php'));
+    }
+  } else {
+    // nothing selected; show the homepage
+  }
+?>
+
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
 <div id="main">
@@ -7,13 +19,20 @@
   <?php include(SHARED_PATH . '/public_navigation.php'); ?>
 
   <div id="page">
+
     <?php
-      // Show the homepage
-      // The homepage content could:
-      //* be static content (here or in a shard file)
-      //* show the first page from the nav
-      //* be in the database but add code to hide in the nav
-      include(SHARED_PATH . '/static_homepage.php');
+      if(isset($page)) {
+        // show the page from the database
+        // TODO add html escaping back in
+        echo $page['content'];
+      } else {
+        // Show the homepage
+        // The homepage content could:
+        //* be static content (here or in a shard file)
+        //* show the first page from the nav
+        //* be in the database but add code to hide in the nav
+        include(SHARED_PATH . '/static_homepage.php');
+      }
     ?>
   </div>
 
