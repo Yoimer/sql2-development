bd34a52f89800cbf56207e5264a649a8d9a656eb
Yoimer <ydrf19@gmail.com>
2018-03-15 12:39:19 -0400

    015 use an option for conditional code

--- public/index.php
+++ public/index.php
@@ -10,8 +10,8 @@
     $subject_id = $page['subject_id'];
   } elseif(isset($_GET['subject_id'])) {
     $subject_id = $_GET['subject_id'];
-    $page_set = find_pages_by_subject_id($subject_id);
-    $page = mysqli_fetch_assoc($page_set); // first page
+    $page_set = find_pages_by_subject_id($subject_id, ['visible' => true]);
+    $page = mysqli_fetch_assoc($page_set); // first page (first visible page)
     mysqli_free_result($page_set);
     if(!$page) {
       redirect_to(url_for('/index.php'));
