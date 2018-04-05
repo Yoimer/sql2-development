d99ce0a709cb7ad9393ada87e93ab0418448866f
Yoimer <ydrf19@gmail.com>
2018-04-05 00:34:01 -0400

    shows username on page if and only if logged in successfully

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -18,7 +18,14 @@
 
     <navigation>
       <ul>
-        <li>User: <?php echo isset($_SESSION['username']) ? $_SESSION['username'] : ''; ?></li>
+        <li>User: <?php   // if there is any log in error do not show the user on page :)
+          if (empty($errors)) {
+            echo isset($_SESSION['username']) ? $_SESSION['username'] : ''; 
+          } else {
+            unset($_SESSION['username']);
+          }
+          ?>
+        </li>
         <li><a href="<?php echo url_for('/staff/index.php'); ?>">Menu</a></li>
         <li><a href="<?php echo url_for('/staff/logout.php'); ?>">Logout</a></li>
       </ul>
