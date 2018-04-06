22ef7a69388658310a279bbf2a57f589b652864d
Yoimer <ydrf19@gmail.com>
2018-04-05 22:50:34 -0400

    035 log out a user

--- public/staff/logout.php
+++ public/staff/logout.php
@@ -1,12 +1,7 @@
 <?php
 require_once('../../private/initialize.php');
 
-unset($_SESSION['username']);
-
-//unset($_SESSION['admin_id']); //I tested this and clean completly the session :) ;) hehe
-
-// or you could use
-// $_SESSION['username'] = NULL;
+log_out_admin();
 
 redirect_to(url_for('/staff/login.php'));
 
