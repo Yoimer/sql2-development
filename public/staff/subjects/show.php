20b8885031bc7d13d5f9fb319c173019010ff420
Yoimer <ydrf19@gmail.com>
2018-04-08 19:39:57 -0400

    039 list pages by subject

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -8,6 +8,7 @@ $id = isset($_GET['id']) ? $_GET['id'] : 'DEFAULT VALUE';
 //$id = $_GET['id'] ?? '1'; // PHP > 7.0 
 //$subject['menu_name'] = isset($_POST['menu_name']) ? $_POST['menu_name'] : 'DEFAULT VALUE';
 $subject = find_subject_by_id($id);
+$page_set = find_pages_by_subject_id($id);
 
 ?>
 
@@ -36,6 +37,47 @@ $subject = find_subject_by_id($id);
         <dd><?php echo $subject['visible'] == '1' ? 'true' : 'false'; ?></dd>
       </dl>
     </div>
+
+    <!-- horinzontal ruler, creates a line across the botton -->
+    <hr />
+
+    <div class="pages listing">
+      <h2>Pages</h2>
+
+      <div class="actions">
+        <a class="action" href="<?php echo url_for('/staff/pages/new.php'); ?>">Create New Page</a>
+      </div>
+
+      <table class="list">
+        <tr>
+          <th>ID</th>
+          <!-- <th>Subject</th> -->
+          <th>Position</th>
+          <th>Visible</th>
+          <th>Name</th>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+        </tr>
+
+        <?php while($page = mysqli_fetch_assoc($page_set)) { ?>
+          <?php $subject = find_subject_by_id($page['subject_id']); ?>
+          <tr>
+            <td><?php echo h($page['id']); ?></td>
+            <!-- <td><?php //echo h($subject['menu_name']); ?></td> -->
+            <td><?php echo h($page['position']); ?></td>
+            <td><?php echo $page['visible'] == 1 ? 'true' : 'false'; ?></td>
+            <td><?php echo h($page['menu_name']); ?></td>
+            <td><a class="action" href="<?php echo url_for('/staff/pages/show.php?id=' . h(u($page['id']))); ?>">View</a></td>
+            <td><a class="action" href="<?php echo url_for('/staff/pages/edit.php?id=' . h(u($page['id']))); ?>">Edit</a></td>
+            <td><a class="action" href="<?php echo url_for('/staff/pages/delete.php?id=' . h(u($page['id']))); ?>">Delete</a></td>
+          </tr>
+        <?php } ?>
+      </table>
+
+      <?php mysqli_free_result($page_set); ?>
+
+      </div>
 
   </div>
 
